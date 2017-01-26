588962767b57e0ade4a948c3017c3bb742e99b6b
Daniel Arteaga <darteaga@us.es>
2017-01-26 12:12:32 +0100

    exported function execCommand

--- index.js
+++ index.js
@@ -1,79 +1,89 @@
 var child_process = require('child_process');
 
-exports.dockerComposeUp = function(dir, options, success, error){
-	var command = 'docker-compose up';
+module.exports.dockerComposeUp = function (dir, options, success, error) {
+    var command = 'docker-compose up';
 
-	if(options){
-		command += ' ' + options;
-	}
+    if (options) {
+        command += ' ' + options;
+    }
 
-	execCommand(command, success, error, {cwd: dir});
+    _execCommand(command, success, error, {
+        cwd: dir
+    });
 }
 
-exports.dockerComposeDown = function(dir, options, success, error){
-	var command = 'docker-compose down';
+module.exports.dockerComposeDown = function (dir, options, success, error) {
+    var command = 'docker-compose down';
 
-	if(options){
-		command += ' ' + options;
-	} 
-	
-	execCommand(command, success, error, {cwd: dir});
+    if (options) {
+        command += ' ' + options;
+    }
 
-}
-
-exports.dockerComposeStop = function(dir, success, error){
-	var command = 'docker-compose stop';
-
-	execCommand(command, success, error, {cwd: dir});
+    _execCommand(command, success, error, {
+        cwd: dir
+    });
 
 }
 
-exports.dockerComposeStart = function(dir, success, error){
-	var command = 'docker-compose start';
+module.exports.dockerComposeStop = function (dir, success, error) {
+    var command = 'docker-compose stop';
 
-	execCommand(command, success, error, {cwd: dir});
-
-}
-
-exports.dockerExec = function (container, exec_command, options, success, error){
-	var command = 'docker exec';
-
-	if(options){
-		for(option in options){
-			command += ' ' + options[option];
-		}
-	}
-
-	command += ' ' + container;
-
-	command += ' ' + exec_command;
-
-	execCommand(command, success, error);
+    _execCommand(command, success, error, {
+        cwd: dir
+    });
 
 }
 
-exports.dockerInspectIPAddressOfContainer = function (container, options){
-	var command = "docker inspect --format '{{.NetworkSettings.Networks." + options.network + ".IPAddress}}' " + container;
+module.exports.dockerComposeStart = function (dir, success, error) {
+    var command = 'docker-compose start';
 
-	return child_process.execSync(command).toString('utf-8').replace(/(?:\r\n|\r|\n)/g, '');
-}
-
-exports.dockerInspectPortOfContainer = function (container, options){
-	var command = "docker inspect --format '{{.NetworkSettings.Ports}}' " + container;
-
-	return child_process.execSync(command).toString('utf-8').replace(/(?:\r\n|\r|\n)/g, '').split("[")[1].split("/")[0];
-}
-
-function execCommand(command, success, error, options){
-
-	child_process.exec(command, options, function(err, stdout, stderr){
-		if(!err){
-			if(success)
-				success(stdout, stderr);
-		}else{
-			if(error)
-				error(err, stderr);	
-		}					
-	});
+    _execCommand(command, success, error, {
+        cwd: dir
+    });
 
 }
+
+module.exports.dockerExec = function (container, exec_command, options, success, error) {
+    var command = 'docker exec';
+
+    if (options) {
+        for (option in options) {
+            command += ' ' + options[option];
+        }
+    }
+
+    command += ' ' + container;
+
+    command += ' ' + exec_command;
+
+    _execCommand(command, success, error);
+
+}
+
+module.exports.dockerInspectIPAddressOfContainer = function (container, options) {
+    var command = "docker inspect --format '{{.NetworkSettings.Networks." + options.network + ".IPAddress}}' " + container;
+
+    return child_process.execSync(command).toString('utf-8').replace(/(?:\r\n|\r|\n)/g, '');
+}
+
+module.exports.dockerInspectPortOfContainer = function (container, options) {
+    var command = "docker inspect --format '{{.NetworkSettings.Ports}}' " + container;
+
+    return child_process.execSync(command).toString('utf-8').replace(/(?:\r\n|\r|\n)/g, '').split("[")[1].split("/")[0];
+}
+
+function _execCommand(command, success, error, options) {
+
+    child_process.exec(command, options, function (err, stdout, stderr) {
+        if (!err) {
+            if (success)
+                success(stdout, stderr);
+        } else {
+            if (error)
+                error(err, stderr);
+        }
+    });
+
+}
+
+module.exports.execCommand = _execCommand;
